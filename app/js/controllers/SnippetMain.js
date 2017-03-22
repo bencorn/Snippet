ce31027e676ff521aa630cf0c5b9304f8f938899
Benjamin Corn <bcorn@bu.edu>
2017-03-21 23:57:14 -0400

    PATCH: Pause Player when New Player Selected
    When new player is selected to play, pause all other players.

--- app/js/controllers/SnippetMain.js
+++ app/js/controllers/SnippetMain.js
@@ -15,4 +15,15 @@ angular.module('SnippetMain', []).controller('SnippetMain', function($scope, $ht
 				vm.SongResults = resp.data;
 			});
 	}
+
+	$(function () {
+	    document.addEventListener('play', function (e) {
+	        var audios = document.getElementsByTagName('audio');
+	        for (var i = 0, len = audios.length; i < len; i++) {
+	            if (audios[i] != e.target) {
+	                audios[i].pause();
+	            }
+	        }
+	    }, true);
+	})
 });
