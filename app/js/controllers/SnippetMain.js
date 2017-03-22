3e13350f99fe1448391bda67b2c1cdddbf433b2f
Benjamin Corn <bcorn@bu.edu>
2017-03-21 21:20:05 -0400

    Refactoring of SnippetMain Controller
    Added comments, changed var names, code clean.

--- app/js/controllers/SnippetMain.js
+++ app/js/controllers/SnippetMain.js
@@ -1,15 +1,18 @@
 angular.module('SnippetMain', []).controller('SnippetMain', function($scope, $http) {
 
+	// View Model
 	vm = this;
 
+	// Search for Songs from Search Query
 	vm.Search = function(){
-		console.log(vm.SearchQuery);
-		payload = {searchQuery: vm.SearchQuery};
+	    payload = {
+	        searchQuery: vm.SearchQuery
+	    };
+        // POST Query to Snippet API
 		$http.post('/api/spotify/search', payload)
-			.then(function(resp){
-				console.log(resp.data);
+			.then(function (resp) {
+                // Assign VM Property Song Results to resulting JSON
 				vm.SongResults = resp.data;
 			});
 	}
-
 });
