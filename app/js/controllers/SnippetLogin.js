21adc74d90dd06e70cd829d26a87c5f32e9618da
namirfa <namirfawaz@Namirs-MacBook-Pro.local>
2017-04-09 19:27:47 -0400

    Facebook Authentication
    commented everything out, its just the copy pasted code from the
    passport page. we also need a callbackURL so we gotta figure that out.

--- app/js/controllers/SnippetLogin.js
+++ app/js/controllers/SnippetLogin.js
@@ -13,7 +13,23 @@ angular.module('SnippetLogin', []).controller('SnippetLogin', function($scope, $
 		
 		$http.post('/api/user/register', req)
 			.then(function(res){
-				// TO BE IMPLEMENTED	
+				/*
+				var passport = require('passport')
+  					, FacebookStrategy = require('passport-facebook').Strategy;
+
+				passport.use(new FacebookStrategy({
+				    clientID: FACEBOOK_APP_ID,
+				    clientSecret: FACEBOOK_APP_SECRET,
+				    callbackURL: "http://www.example.com/auth/facebook/callback"
+				  },
+				  function(accessToken, refreshToken, profile, done) {
+				    User.findOrCreate(..., function(err, user) {
+				      if (err) { return done(err); }
+				      done(null, user);
+				    });
+				  }
+				));
+			*/
 			});
 	}
 	
